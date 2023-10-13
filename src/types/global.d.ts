4819634b576ac37da360f5835931f667c154d0cc
Fantasy-Ke <2246080525@qq.com>
2023-10-13 20:22:10 +0800

    错误全局引用

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -2,12 +2,12 @@
 /**
  * 扩展全局属性
  */
-declare module 'vue' {
-    interface ComponentCustomProperties {
-        //格式化日期
-        $formatDate: (value: string, format: string = "YYYY-MM-DD HH:mm:ss") => string
-    }
-}
+// declare module 'vue' {
+//     interface ComponentCustomProperties {
+//         //格式化日期
+//         $formatDate: (value: string, format: string = "YYYY-MM-DD HH:mm:ss") => string
+//     }
+// }
 
 declare interface Window {
   nextLoading: boolean;
